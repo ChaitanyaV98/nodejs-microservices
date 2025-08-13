8187ad3c5dbf72c9823276d44c4444203ebf76c9
ChaitanyaV98 <chaitanya@betalectic.com>
2025-08-13 14:49:44 +0530

    added comments for refresh token modal

--- identity-service/src/models/RefreshToken.js
+++ identity-service/src/models/RefreshToken.js
@@ -20,6 +20,17 @@ const refreshTokenSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// understandinf: When you insert a refresh token with an expiresAt timestamp (e.g., 30 days from now),
+
+//MongoDB automatically deletes it as soon as that time is hit — no manual cleanup needed!
+
+// example : {
+//   token: "abc123",
+//   user: ObjectId("..."),
+//   expiresAt: "2025-08-16T10:00:00Z"
+// }
+
+//At "2025-08-16T10:00:00Z", MongoDB will remove this document automatically.
 refreshTokenSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
 
 const RefreshToken = mongoose.model("refreshToken", refreshTokenSchema);
