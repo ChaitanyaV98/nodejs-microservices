4dc2bbc2aa9697c3158e70aa1532ce561fd9a354
ChaitanyaV98 <chaitanya@betalectic.com>
2025-08-13 15:31:20 +0530

    updated routes for post

--- post-service/src/routes/post-routes.js
+++ post-service/src/routes/post-routes.js
@@ -8,10 +8,10 @@ import {
 } from "../controllers/post-controller.js";
 
 const router = express.Router();
-
+//here authenticateRequest method is used to tell if the user is an auth user or not
 router.post("/create-post", authenticateRequest, createPost);
 router.get("/all-posts", authenticateRequest, getAllPosts);
-router.get("/single-post", authenticateRequest, getPost);
-router.get("/delete-post", authenticateRequest, deletePost);
+router.get("/single-post/:id", authenticateRequest, getPost);
+router.delete("/:id", authenticateRequest, deletePost);
 
 export default router;
